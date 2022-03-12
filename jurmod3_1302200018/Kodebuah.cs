7fe60968aaee4926fcf045533347159a5071efd5
abdulghofur12 <ghofursans12@gmail.com>
2022-03-12 10:13:58 +0700

    Add project files.

---
+++ jurmod3_1302200018/Kodebuah.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurmod3_1302200018
+{
+    internal class Kodebuah
+    {
+        public enum NamaBuah
+        {
+            Apel,
+            Aprikot,
+            Alpukat,
+            Pisang,
+            Paprika,
+            Blackberry,
+            Ceri,
+            Kelapa,
+            Jagung,
+            Kurma,
+            Durian,
+            Anggur,
+            Melon,
+            Semangka
+        };
+
+
+
+        public enum KodeBuah
+        {
+
+        }
+        
+    }
+}
